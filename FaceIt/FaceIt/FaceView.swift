5c534ed40fd8996051bce818ad48ff32b0743773
Zombiechimp <teajakic@icloud.com>
2016-12-07 22:59:27 +0100

    FaceIt
    FaceIt made while watching Stanford Swift lectures

---
+++ FaceIt/FaceIt/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by CS193p Instructor.
+//  Copyright © 2016 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView
+{
+        override func draw(_ rect: CGRect)
+    {
+        
+        
+    }
+    
+    
+    }
+
---
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Tea Jakić on 04/10/2016.
+//  Copyright © 2016 Tea Jakić. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+    
+
+}
+
